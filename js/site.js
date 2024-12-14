b333913c914dc6ef5bcf2cd6c605e5091dcb1e0f
dreamstrata <craftaxis@gmail.com>
2024-12-13 22:13:50 -0500

    update

--- js/site.js
+++ js/site.js
@@ -1,4 +1,32 @@
+var pages = ['home','art','wpaper','shop'];
 $(document).ready(function(){
         const currentYear = new Date().getFullYear();
         $('.footerdate').html(currentYear);
+
+        for(let i = 0; i < pages.length; i++) {
+            $('#'+pages[i]+'button').click(function(){
+                showPage($(this).attr('data'));
+            });
+        }
 });
+function showPage(page)
+{
+        for(let i = 0; i < pages.length; i++) {
+            $('#'+pages[i]+'page').css(
+                {
+                        'display' : 'none',
+                });
+                $('#'+pages[i]+'button').css(
+                {
+                        'cursor' : 'pointer',
+                        'pointer-events' : 'auto',
+                        'color' : 'inherit',
+                });
+        }
+        $('#'+page+'page').css('display', 'block');
+        $('#'+page+'button').css({
+                'cursor' : 'default',
+                'pointer-events' : 'none',
+                'color' : '#F00',
+        });
+}
