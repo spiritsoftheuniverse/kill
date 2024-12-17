dcf2c3485972f23a4a1e9e264d7fd7ca087ad77d
dreamstrata <craftaxis@gmail.com>
2024-12-17 12:14:57 -0500

    update

--- js/art.js
+++ js/art.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
         parseArtCSV('categories');
+        parseArtCSV('art_today');
 })
 function parseArtCSV(file) {
         Papa.parse('https://spiritsoftheuniverse.github.io/kill/data/' + file+'.csv', {
@@ -15,7 +16,10 @@ function parseArtCSV(file) {
             });
             switch (file) {
                 case 'categories':	
-                        console.log(dataObject);
+                        writeCategories(dataObject);
+                break;
+                case 'art_today' : 
+                        writeArtThumbs('today', dataObject);
                 break;
                 default:
                 break;
@@ -26,3 +30,42 @@ function parseArtCSV(file) {
           }
         });
       }
+function writeArtThumbs(category, data)
+{
+        const count = Object.keys(data).length;
+        var html = '';
+        for(let i = 0; i < count; i++) {
+                html += '<img src="images/art/'+data[0][0]+'t/">';
+        }
+        console.log(html);
+        $('.artthumbbox[data-folder="'+category+'"] .artthumbcontainer').html(html);
+}
+function writeCategories(data)
+{
+        const count = Object.keys(data).length;
+        console.log(data);
+        var html = '';
+        for(let i = 0; i < count; i++) {
+                var description = '';
+                if(data[i][2] != '')
+                {
+                        description = `<div style="margin-bottom:8px;">`+data[i][2]+`</div>`;
+                }
+            html += `<div class="arthead" data-folder="`+data[i][0]+`">`+data[i][1]+`</div>
+                
+                <div class="artthumbbox" data-folder="`+data[i][0]+`">`+description+`<div class="artthumbcontainer"></div></div>
+            `;
+        }
+        $('#artpage').html(html);
+
+        $('.arthead').click(function(){
+                showArtThumbs($(this).attr('data-folder'));
+        })
+}
+function showArtThumbs(category)
+{
+        $('.artthumbbox').css('display', 'none');
+        $('.arthead').css('color', 'inherit');
+        $('.arthead[data-folder="'+category+'"]').css('color', '#FA0');
+        $('.artthumbbox[data-folder="'+category+'"]').css('display', 'flex');
+}
